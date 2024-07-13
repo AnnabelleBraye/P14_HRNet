c8dde0fb28a4bddddf8d3686b388ef238cb833e7
Annabelle <annabelle.braye@gmail.com>
2024-07-13 15:07:02 +0200

    Updates

--- src/types/react-table-component-package/index.d.ts
+++
@@ -1,8 +0,0 @@
-// types/react-table-component-package/index.d.ts
-declare module 'react-table-component-package' {
-  import { FC } from 'react';
-  import { TableProps, ColType } from 'react-table-component-package/dist/react-table-component-package.es.js';
-
-  export const Table: FC<TableProps>;
-  export { TableProps, ColType };
-}
